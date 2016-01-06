00a22a965366d7705c6978da091eae47ba55beb6
gentilmente <egentilemontes@gmail.com>
2016-01-05 22:42:51 -0300

    agregado de animación CSS ejecutada desde JS (surge necesidad de Async js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 /* botones-js-CSS.js */
+var guerra = document.getElementById("guerra");
+
+function pacificar(){
+	guerra.innerHTML = "Paz";
+}
+
+function mejorarPacificacion () {
+	//animación por ejemplo fade out fade in, etc
+	guerra.classList.add('fadeout');
+	pacificar();
+
+}
 function descontaminar() {
 
 }
