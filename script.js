96d66f8477568de0bd9cb67f8b8669f132375bbb
gentilmente <egentilemontes@gmail.com>
2016-01-07 18:08:36 -0300

    setTimeout(), asincronismo

--- script.js
+++ script.js
@@ -2,15 +2,28 @@
 var guerra = document.getElementById("guerra");
 
 function pacificar(){
+  //guerra.classList.remove('fadeout', 'fadein');
 	guerra.innerHTML = "Paz";
 }
 
 function mejorarPacificacion () {
-	//animación por ejemplo fade out fade in, etc
-	guerra.classList.add('fadeout');
-	pacificar();
+  //animación por ejemplo fade out fade in, etc
+  guerra.classList.add('fadeout');
 
+  //una manera de usar la func:
+  setTimeout(function() {
+    pacificar();
+  }, 1000);
+  
+  //otra manera sólo si no usa (argumentos):
+  setTimeout(fadein, 1000);
 }
+
+function fadein() {
+  guerra.classList.add('fadein');
+}
+
+
 function descontaminar() {
 
 }
